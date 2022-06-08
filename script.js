f0c982176c6c2ebccd7aa9961afee94d1d6a5093
12Gustavo21 <gustavo927318@gmail.com>
2022-06-08 10:55:16 -0300

    Desafio-de-logica01

---
+++ script.js
@@ -0,0 +1,18 @@
+let nome = "Gustavo";
+console.log(nome);
+
+let numero = 17;
+console.log(numero);
+
+let ano = 2022;
+console.log(ano - numero);
+
+let soma = ano + numero;
+console.log(soma);
+
+let mensagem = "Estou mexendo no console.log";
+console.log(mensagem);
+
+let quartaFeira = false;
+console.log(quartaFeira);
+console.log(typeof quartaFeira);
